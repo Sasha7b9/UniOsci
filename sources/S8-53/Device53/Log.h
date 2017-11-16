b37211831d97ab9936dcc1b8167d9856034413d4
Sasha7b9 <Sasha7b9@tut.by>
2017-11-16 12:19:49 +0300

    *

--- sources/S8-53/Device53/Log.h
+++ sources/S8-53/Device53/Log.h
@@ -1,12 +1,23 @@
 #pragma once   
 #include "defines.h"
 
+#ifdef WIN32
+
+#define LOG_WRITE(...)
+#define LOG_ERROR(...)
+#define LOG_FUNC_ENTER
+#define LOG_FUNC_LEAVE
+#define LOG_TRACe
+
+#else
 
 #define LOG_WRITE(...)  Log_Write(__VA_ARGS__)
 #define LOG_ERROR(...)  Log_Error(__MODULE__, __FUNCTION__, __LINE__, __VA_ARGS__)
 #define LOG_FUNC_ENTER  Log_Write("%s enter", __FUNCTION__);
 #define LOG_FUNC_LEAVE  Log_Write("%s leave", __FUNCTION__);
 #define LOG_TRACE       Log_Write("%s : %d", __MODULE__, __LINE__);
+
+#endif
 
 void Log_Error(const char *module, const char *func, int numLine, char *format, ...);
 void Log_Write(char *format, ...);
