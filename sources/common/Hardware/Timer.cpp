1db052c5e230553fa4acd9fcae1b72320c618954
Sasha7b9 <Sasha7b9@tut.by>
2017-12-11 17:45:11 +0300

    *

--- sources/common/Hardware/Timer.cpp
+++ sources/common/Hardware/Timer.cpp
@@ -51,7 +51,6 @@ void Timer::Init()
 
     __HAL_RCC_TIM2_CLK_ENABLE();    // Для тиков
     __HAL_RCC_TIM3_CLK_ENABLE();    // Для таймеров
-    __HAL_RCC_TIM5_CLK_ENABLE();    // Для миллисекунд
 
     HAL_NVIC_EnableIRQ(TIM3_IRQn);
     HAL_NVIC_SetPriority(TIM3_IRQn, 0, 1);
@@ -62,23 +61,19 @@ void Timer::Init()
     handleTIM3.Init.Period = 1;
     handleTIM3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 
-    TIM_HandleTypeDef handleTIM2;
-    handleTIM2.Instance = TIM2;
-    handleTIM2.Init.Prescaler = 0;
-    handleTIM2.Init.CounterMode = TIM_COUNTERMODE_UP;
-    handleTIM2.Init.Period = (uint)-1;
-    handleTIM2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    TIM_HandleTypeDef handleTIM2 =
+    {
+        TIM2,
+        {
+            0,
+            TIM_COUNTERMODE_UP,
+            (uint)-1,
+            TIM_CLOCKDIVISION_DIV1
+        }
+    };
+
     HAL_TIM_Base_Init(&handleTIM2);
     HAL_TIM_Base_Start(&handleTIM2);
-
-    TIM_HandleTypeDef handleTIM5;
-    handleTIM5.Instance = TIM5;
-    handleTIM5.Init.Prescaler = 44999;
-    handleTIM5.Init.CounterMode = TIM_COUNTERMODE_UP;
-    handleTIM5.Init.Period = (uint) -1;
-    handleTIM5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-    HAL_TIM_Base_Init(&handleTIM5);
-    HAL_TIM_Base_Start(&handleTIM5);
 }
 
 #ifdef __cplusplus
@@ -94,110 +89,6 @@ void TIM3_IRQHandler()
 #ifdef __cplusplus
 }
 #endif
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-void Timer::Set(TypeTimer2 type, pFuncVV func, uint dTms)
-{
-    TimerStruct *timer = &timers[type];
-    timer->func = func;
-    timer->dTms = dTms;
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-void Timer::SetAndStartOnce(TypeTimer2 type, pFuncVV func, uint dTms)
-{
-    Timer::Set(type, func, dTms);
-    StartOnce(type);
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-void Timer::SetAndEnable(TypeTimer2 type, pFuncVV func, uint dTms)
-{
-    Set(type, func, dTms);
-    Enable(type);
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-void Timer::StartOnce(TypeTimer2 type)
-{
-    timers[type].repeat = false;
-    TuneTIM(type);
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-void Timer::Enable(TypeTimer2 type)
-{
-    timers[type].repeat = true;
-    TuneTIM(type);
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-static void TuneTIM(TypeTimer2 type)
-{
-    TimerStruct *timer = &timers[type];
-    timer->timeFirstMS = gTimeMS;
-
-    uint timeNearest = NearestTime();
-
-    uint timeNext = timer->timeFirstMS + timer->dTms;
-    timer->timeNextMS = timeNext;
-
-    if(timeNext < timeNearest)      // Если таймер должен сработать раньше текущего
-    {
-        StartTIM(timeNext);         // то заводим таймер на наше время
-    }
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-void Timer::Disable(TypeTimer2 type)
-{
-    timers[type].timeNextMS = UINT_MAX;
-    timers[type].repeat = false;
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-static uint NearestTime()
-{
-    uint time = UINT_MAX;
-
-    for(uint type = 0; type < NumTimers; type++)
-    {
-        if(TIME_NEXT(type) < time)
-        {
-            time = TIME_NEXT(type);
-        }
-    }
-    
-    return time;
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-static void StartTIM(uint timeStopMS)
-{
-    StopTIM();
-
-    if(timeStopMS == UINT_MAX)
-    {
-        return;
-    }
-
-    uint dT = timeStopMS - gTimeMS;
-
-    handleTIM3.Init.Period = (dT * 2) - 1;      // 10 соответствует 0.1мс. Т.е. если нам нужна 1мс, нужно засылать (100 - 1)
-
-    HAL_TIM_Base_Init(&handleTIM3);
-    HAL_TIM_Base_Start_IT(&handleTIM3);
-}
-
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *)
@@ -232,6 +123,99 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *)
     StartTIM(NearestTime());
 }
 
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void Timer::Set(TypeTimer2 type, pFuncVV func, uint dTms)
+{
+    TimerStruct *timer = &timers[type];
+    timer->func = func;
+    timer->dTms = dTms;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void Timer::SetAndStartOnce(TypeTimer2 type, pFuncVV func, uint dTms)
+{
+    Timer::Set(type, func, dTms);
+    StartOnce(type);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void Timer::SetAndEnable(TypeTimer2 type, pFuncVV func, uint dTms)
+{
+    Set(type, func, dTms);
+    Enable(type);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void Timer::StartOnce(TypeTimer2 type)
+{
+    timers[type].repeat = false;
+    TuneTIM(type);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void Timer::Enable(TypeTimer2 type)
+{
+    timers[type].repeat = true;
+    TuneTIM(type);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+static void TuneTIM(TypeTimer2 type)
+{
+    TimerStruct *timer = &timers[type];
+    timer->timeFirstMS = gTimeMS;
+
+    uint timeNearest = NearestTime();
+
+    uint timeNext = timer->timeFirstMS + timer->dTms;
+    timer->timeNextMS = timeNext;
+
+    if(timeNext < timeNearest)      // Если таймер должен сработать раньше текущего
+    {
+        StartTIM(timeNext);         // то заводим таймер на наше время
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+void Timer::Disable(TypeTimer2 type)
+{
+    timers[type].timeNextMS = UINT_MAX;
+    timers[type].repeat = false;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+static uint NearestTime()
+{
+    uint time = UINT_MAX;
+
+    for(uint type = 0; type < NumTimers; type++)
+    {
+        if(TIME_NEXT(type) < time)
+        {
+            time = TIME_NEXT(type);
+        }
+    }
+    
+    return time;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+static void StartTIM(uint timeStopMS)
+{
+    StopTIM();
+
+    if(timeStopMS == UINT_MAX)
+    {
+        return;
+    }
+
+    uint dT = timeStopMS - gTimeMS;
+
+    handleTIM3.Init.Period = (dT * 2) - 1;      // 10 соответствует 0.1мс. Т.е. если нам нужна 1мс, нужно засылать (100 - 1)
+
+    HAL_TIM_Base_Init(&handleTIM3);
+    HAL_TIM_Base_Start_IT(&handleTIM3);
+}
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 static void StopTIM()
@@ -239,13 +223,11 @@ static void StopTIM()
     HAL_TIM_Base_Stop_IT(&handleTIM3);
 }
 
-
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 void Timer::PauseOnTime(uint timeMS)
 {
     HAL_Delay(timeMS);
 }
-
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 void Timer::PauseOnTicks(uint numTicks)
@@ -256,7 +238,6 @@ void Timer::PauseOnTicks(uint numTicks)
     };
 }
 
-
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 void Timer::StartMultiMeasurement()
 {
@@ -265,14 +246,12 @@ void Timer::StartMultiMeasurement()
     TIM2->CR1 |= TIM_CR1_CEN;
 }
 
-
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 void Timer::StartLogging()
 {
     timeStartLogging = gTimerTics;
     timePrevPoint = timeStartLogging;
 }
-
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 uint Timer::LogPointUS(char *name)
@@ -282,7 +261,6 @@ uint Timer::LogPointUS(char *name)
     LOG_WRITE("%s %.2f us", name, interval / 120.0f);
     return interval;
 }
-
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 uint Timer::LogPointMS(char *name)
